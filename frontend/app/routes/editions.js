41da83de819f9d48680aebcdb616da2b3214f057
Galen Palmer <palmergs@gmail.com>
2016-07-09 20:12:57 -0400

    basic index queries for edition and work

--- frontend/app/routes/editions.js
+++ frontend/app/routes/editions.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    ident: { refreshModel: true },
+    title: { refreshModel: true },
+    subtitle: { refreshModel: true },
+    lcc: { refreshModel: true },
+    pages: { refreshModel: true },
+    publishDate: { refreshModel: true },
+    format: { refreshModel: true },
+    createdAt: { refreshModel: true }
+  },
+
+  model(params) {
+    return this.store.query('edition', params);
+  }
 });
--- frontend/app/routes/works.js
+++ frontend/app/routes/works.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    ident: { refreshModel: true },
+    title: { refreshModel: true },
+    subtitle: { refreshModel: true },
+    publishDate: { refreshModel: true },
+    editionsCount: { refreshModel: true },
+    lcc: { refreshModel: true },
+    createdAt: { refreshModel: true }
+  },
+
+  model(params) {
+    return this.store.query('work', params);
+  }
 });
