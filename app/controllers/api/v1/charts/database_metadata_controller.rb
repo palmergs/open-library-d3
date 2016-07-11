160e56ce1da125d16faee3ee56895e64d393cef4
Galen Palmer <palmergs@gmail.com>
2016-07-11 16:48:40 -0400

    add endpoint for tag table sizes; add low level cache for restuls

--- app/controllers/api/v1/charts/database_metadata_controller.rb
+++ app/controllers/api/v1/charts/database_metadata_controller.rb
@@ -2,25 +2,39 @@ class Api::V1::Charts::DatabaseMetadataController < ApplicationController
 
   def main_tables
 
-    data = [
-      { label: 'Work', value: Work.count },
-      { label: 'Author', value: Author.count },
-      { label: 'Edition', value: Edition.count }
-    ]
+    data = Rails.cache.fetch("database-metadata/main-tables", expires_in: 1.hour) do
+      [
+        { label: 'Work', value: Work.count },
+        { label: 'Author', value: Author.count },
+        { label: 'Edition', value: Edition.count }
+      ]
+    end
     render json: data
   end
 
   def all_tables
     
-    data = [
-      { label: 'Work', value: Work.count },
-      { label: 'Author', value: Author.count },
-      { label: 'Edition', value: Edition.count },
-      { label: 'Edition Publisher', value: EditionPublisher.count },
-      { label: 'Work Author', value: WorkAuthor.count },
-      { label: 'Tag', value: SubjectTag.count },
-      { label: 'Link', value: ExternalLink.count }
-    ]
+    data = Rails.cache.fetch("database-metadata/all-tables", expires_in: 1.hour) do
+      [
+        { label: 'Work', value: Work.count },
+        { label: 'Author', value: Author.count },
+        { label: 'Edition', value: Edition.count },
+        { label: 'Work Author', value: WorkAuthor.count },
+        { label: 'Tag', value: SubjectTag.count }
+      ]
+    end
+    render json: data
+  end
+
+  def tag_tables
+
+    data = Rails.cache.fetch("database-metadata/tag-tables", expires_in: 1.hour do
+      [
+        { label: 'Work Tags', value: SubjectTag.where(taggable_type: 'Work').count },
+        { label: 'Author Tags', value: SubjectTag.where(taggable_type: 'Author').count },
+        { label: 'Edition Tags', value: SubjectTag.where(taggable_type: 'Edition').count }
+      ]
+    end
     render json: data
   end
 end
