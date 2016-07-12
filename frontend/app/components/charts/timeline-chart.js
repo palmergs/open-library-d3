1417790c99b8f782a5ac2fcda87b73d9619f5d7a
Galen Palmer <palmergs@gmail.com>
2016-07-12 16:34:52 -0400

    add axis to timeline; formatting for label; label below chart in template

--- frontend/app/components/charts/timeline-chart.js
+++ frontend/app/components/charts/timeline-chart.js
@@ -16,43 +16,59 @@ export default Ember.Component.extend({
   didInsertElement() {
     this._super(...arguments);
 
-    const width = this.get('width') || this.$().width(), // default to element width
-      height = this.get('height') || (width / 4); // default to 4:1 ratio height to width
+    const margin = { top: 20, right: 90, bottom: 30, left: 40 },
+      width = (this.get('width') || this.$().width()) - margin.left - margin.right, // default to element width
+      height = (this.get('height') || (width / 4)) - margin.top - margin.bottom; // default to 4:1 ratio height to width
+
+    const x = d3.scaleBand().rangeRound([0, width]);
 
     const y = d3.scaleLinear().range([ height, 0 ]);
 
     const path = this.get('path');
     if(path) {
 
-      const svg = d3.select(this.$().get(0)).
+      const chart = d3.select(this.$().get(0)).
         append('svg').
-        attr('width', width).
-        attr('height', height);
+        attr('width', width + margin.left + margin.right).
+        attr('height', height + margin.top + margin.bottom).
+        append('g').
+          attr('transform', 'translate('+ margin.left +','+ margin.top +')');
+
+      
 
       Ember.$.ajax(path).done(function(csv) {
 
         const data = d3.csvParse(csv);
-        const max = d3.max(data, function(d) { console.log(d.Count); return +d.Count; });
+        const max = d3.max(data, function(d) { return +d.Count; });
 
         y.domain([ 0, max ]);
+        x.domain(data.map(function(d) { return +d.Decade; }));
 
-        const barWidth = width / data.length;
+        const xAxis = d3.axisBottom().
+          scale(x).
+          tickValues(x.domain().filter(function(d, i) { return !(i % 2); }));
+        chart.append('g').attr('class', 'x axis').
+          attr('transform', 'translate(0,'+ height +')').
+          call(xAxis).
+          selectAll('text').
+            attr('y', 9).
+            attr('x', 4).
+            attr('dy', '.35em').
+            attr('transform', 'rotate(45)').
+            style('text-anchor', 'start');
 
-        const bar = svg.selectAll('g').
+        const yAxis = d3.axisRight().scale(y);
+        chart.append('g').attr('class', 'y axis').attr('transform', 'translate('+ width +',0)').call(yAxis);
+
+        chart.selectAll('.bar').
           data(data).
-          enter().append('g').
-            attr('transform', function(d, i) { return "translate("+ i * barWidth + ',0)'; });
+          enter().append('rect').
+            attr('class', 'bar').
+            attr('x', function(d) { return x(+d.Decade); }).
+            attr('y', function(d) { return y(+d.Count); }).
+            attr('height', function(d) { return height - y(+d.Count); }).
+            attr('width', x.bandwidth());
 
-        bar.append('rect').
-          attr('y', function(d) { return y(+d.Count); }).
-          attr('height', function(d) { return height - y(+d.Count) + 3; }).
-          attr('width', barWidth - 1);
-
-        bar.append('text').
-          attr('x', barWidth / 2).
-          attr('y', function(d) { return y(+d.Count) + 3; }).
-          attr('dy', '.75em').
-          text(function(d) { return +d.Count; });
       });
     }
   }
