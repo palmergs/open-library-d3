2643ef91364e3d8aaaa3690f42b356438df5e6b0
Galen Palmer <palmergs@gmail.com>
2016-07-11 21:22:52 -0400

    add cache to timeline values

--- app/controllers/api/v1/charts/author_controller.rb
+++ app/controllers/api/v1/charts/author_controller.rb
@@ -3,18 +3,34 @@ require 'csv'
 class Api::V1::Charts::AuthorController < ApplicationController
 
   def birth_timeline
-    @decades = Author.select('((birth_date / 10) * 10) as birth_decade, count(*) as count').
-        where('birth_date between 32 and 2016').
-        group('birth_decade').
-        order('birth_decade asc') 
-    csv = CSV.generate do |table|
-      table << [ 'Decade', 'Count' ]
-      @decades.each do |decade|
-
-        table << [ decade.birth_decade, decade.count ]
+    csv = Rails.cache.fetch("author/birth-timeline", expires_in: 1.minute) do
+      decades = Author.select('((birth_date / 10) * 10) as birth_decade, count(*) as count').
+          where('birth_date between 1000 and 2016').
+          group('birth_decade').
+          order('birth_decade asc') 
+      CSV.generate do |table|
+        table << [ 'Decade', 'Count' ]
+        append_with_empty(table, decades, :birth_decade, :count, 1000, 10, 2020)
       end
     end
-
     render text: csv
+  end
+
+  # TODO: make this a concern
+  def append_with_empty table, query, time_method, count_method, start = 1000, increment = 10, complete = 2020
+    last = start
+    query.each do |entry|
+      time = entry.public_send(time_method.to_sym)
+      while last.nil? || last < time
+        table << [ last, 0 ]
+        last = last + increment
+      end
+      table << [ time, entry.public_send(count_method.to_sym) ]
+      last = time + increment
+    end
+    while last.nil? || last < complete
+      table << [ last, 0 ]
+      last = last + increment
+    end
   end
 end
--- app/controllers/api/v1/charts/edition_controller.rb
+++ app/controllers/api/v1/charts/edition_controller.rb
@@ -3,17 +3,18 @@ require 'csv'
 class Api::V1::Charts::EditionController < ApplicationController
 
   def publish_timeline
-    @decades = Edition.select('((publish_date / 10) * 10) as publish_decade, count(*) as count').
-        where('publish_date between 32 and 2016').
-        group('publish_decade').
-        order('publish_decade asc')
-    csv = CSV.generate do |table|
-      table << [ 'Decade', 'Count' ]
-      @decades.each do |decade|
-        table << [ decade.publish_decade, decade.count ]
+    csv = Rails.cache.fetch("edition/publish-timeline", expires_in: 1.hour) do
+      decades = Edition.select('((publish_date / 10) * 10) as publish_decade, count(*) as count').
+          where('publish_date between 1000 and 2016').
+          group('publish_decade').
+          order('publish_decade asc')
+      CSV.generate do |table|
+        table << [ 'Decade', 'Count' ]
+        decades.each do |decade|
+          table << [ decade.publish_decade, decade.count ]
+        end
       end
     end
-
     render text: csv
   end
 end
