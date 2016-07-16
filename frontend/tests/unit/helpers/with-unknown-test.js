09539e28cb368993bc1990f93746df1b98c70c14
Galen Palmer <palmergs@gmail.com>
2016-07-16 18:00:34 -0400

    helper for empty values

---
+++ frontend/tests/unit/helpers/with-unknown-test.js
@@ -0,0 +1,10 @@
+import { withUnknown } from 'frontend/helpers/with-unknown';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | with unknown');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = withUnknown([42]);
+  assert.ok(result);
+});
