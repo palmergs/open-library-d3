c757a0d708557dc8ae2c91790e1805d233b5935f
Galen Palmer <palmergs@gmail.com>
2016-07-16 17:58:31 -0400

    show page for works

--- frontend/app/routes/works.js
+++ frontend/app/routes/works.js
@@ -2,13 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   queryParams: {
-    ident: { refreshModel: true },
-    title: { refreshModel: true },
-    subtitle: { refreshModel: true },
-    publishDate: { refreshModel: true },
-    editionsCount: { refreshModel: true },
-    lcc: { refreshModel: true },
-    createdAt: { refreshModel: true }
+    q: { refreshModel: true },
+    y: { refreshModel: true },
+    p: { refreshModel: true },
+    o: { refreshModel: true },
+    d: { refreshModel: true },
   },
 
   model(params) {
