8cdcc5f22fa3537c8baf59dd668f0dcdd4b13550
Galen Palmer <palmergs@gmail.com>
2016-07-16 17:57:05 -0400

    show page for editions

--- frontend/app/controllers/editions.js
+++ frontend/app/controllers/editions.js
@@ -1,5 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: [ 'ident', 'title', 'subtitle', 'lcc', 'pages', 'publishDate', 'format', 'createdAt' ]
+  queryParams: [ 'q', 'y', 'p', 'o', 'd' ],
+
+  q: null,
+  y: null,
+  p: null,
+  o: null,
+  d: null,
+
+  actions: {
+    setPage(val) { 
+      if(Ember.isEmpty(val) || isNaN(parseInt(val))) { 
+        this.set('p', null);
+      } else {
+        this.set('p', parseInt(val));
+      }
+    }
+  }
 });
--- frontend/app/routes/editions.js
+++ frontend/app/routes/editions.js
@@ -2,14 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   queryParams: {
-    ident: { refreshModel: true },
-    title: { refreshModel: true },
-    subtitle: { refreshModel: true },
-    lcc: { refreshModel: true },
-    pages: { refreshModel: true },
-    publishDate: { refreshModel: true },
-    format: { refreshModel: true },
-    createdAt: { refreshModel: true }
+    q: { refreshModel: true },
+    y: { refreshModel: true },
+    p: { refreshModel: true },
+    o: { refreshModel: true },
+    d: { refreshModel: true },
   },
 
   model(params) {
