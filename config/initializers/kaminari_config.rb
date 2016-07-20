7289706c485bca403e16678bf6fe52357a074c23
Galen Palmer <palmergs@gmail.com>
2016-07-20 12:50:37 -0400

    add a stats query for count

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,3 +1,22 @@
+module Kaminari
+  module PageScopeMethods
+    def total_count
+      @_hacked_total_count ||= begin
+        if self.to_sql =~ /[Ww][Hh][Ee][Rr][Ee]/
+          super
+        else
+
+          # Special case where no query params are selected
+          self.connection.execute("SELECT (reltuples)::integer from pg_class r WHERE relkind='r' AND relname='#{ table_name }'").
+                                  first["reltuples"].
+                                  to_i
+        end
+      end
+    end
+  end
+end
+
+
 Kaminari.configure do |config|
   config.default_per_page = 25
   # config.max_per_page = nil
@@ -8,3 +27,5 @@ Kaminari.configure do |config|
   # config.page_method_name = :page
   # config.param_name = :p
 end
+
+
