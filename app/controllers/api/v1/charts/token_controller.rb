e3b2249a9bd721d5d41d167cc793fd3a6f2c192f
Galen Palmer <palmergs@gmail.com>
2016-07-21 09:19:38 -0400

    allow for params in token chart

--- app/controllers/api/v1/charts/token_controller.rb
+++ app/controllers/api/v1/charts/token_controller.rb
@@ -7,34 +7,52 @@ class Api::V1::Charts::TokenController < ApplicationController
 
 
     tokens = Array(params[:q])
+    now = Date.today.year
+    end_year = default_or_int(params[:e], 1, now, now)
+    start_year = default_or_int(params[:y], 1, end_year, 1000)
+    diff = end_year - start_year
+    slice = [[10, diff / 100].min, 1].max
+
     header = [ 'Decade' ]
     if tokens.empty?
+      
 
       header << 'Count' 
-      all = Token.by_type(params[:t]).by_category(params[:c]).select('((year / 10) * 10) as decade, count(*) as count').
-          where('year between 1000 and 2016').
+      all = Token.by_type(params[:t]).by_category(params[:c]).select("((year / #{ slice }) * #{ slice }) as decade, count(*) as count").
+          where("year between #{ start_year } and #{ end_year }").
           group('decade').
           order('decade asc')
       csv = CSV.generate do |table|
         table << header
-        pad_table(table, [ all ])
+        pad_table(table, [ all ], start: start_year, increment: slice, complete: end_year)
       end
       render text: csv
     else
 
       queries = tokens.each_with_object([]) do |token, array|
         header << token
-        array << Token.by_type(params[:t]).by_category(params[:c]).select('((year / 10) * 10) as decade, sum(count) as count').
+        array << Token.by_type(params[:t]).by_category(params[:c]).select("((year / #{ slice }) * #{ slice }) as decade, sum(count) as count").
             where(token: Token.normalize(token)).
-            where('year between 1000 and 2016').
+            where("year between #{ start_year } and #{ end_year }").
             group('decade').
             order('decade asc')
       end
       csv = CSV.generate do |table|
         table << header
-        pad_table(table, queries)
+        pad_table(table, queries, start: start_year, increment: slice, complete: end_year)
       end
       render text: csv
     end
   end
+
+  private
+
+    def default_or_int val, min, max, default = nil 
+      n = Integer(val)
+      return min if min && n < min
+      return max if max && n > max
+      return n
+    rescue Exception => e
+      default
+    end
 end
