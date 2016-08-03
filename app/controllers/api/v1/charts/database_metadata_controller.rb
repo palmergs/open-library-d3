e72012a88d7eda509cf09c4646a97bf8c7776742
Galen Palmer <palmergs@gmail.com>
2016-08-03 10:24:59 -0400

    increase cache time for static values

--- app/controllers/api/v1/charts/database_metadata_controller.rb
+++ app/controllers/api/v1/charts/database_metadata_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Charts::DatabaseMetadataController < ApplicationController
 
   def main_tables
 
-    data = Rails.cache.fetch("database-metadata/main-tables", expires_in: 8.hour) do
+    data = Rails.cache.fetch("database-metadata/main-tables", expires_in: 7.days) do
       [
         { label: 'Work', value: Work.count(:id) },
         { label: 'Author', value: Author.count(:id) },
@@ -14,7 +14,7 @@ class Api::V1::Charts::DatabaseMetadataController < ApplicationController
 
   def all_tables
     
-    data = Rails.cache.fetch("database-metadata/all-tables", expires_in: 8.hour) do
+    data = Rails.cache.fetch("database-metadata/all-tables", expires_in: 7.days) do
       [
         { label: 'Work', value: Work.count(:id) },
         { label: 'Author', value: Author.count(:id) },
@@ -29,7 +29,7 @@ class Api::V1::Charts::DatabaseMetadataController < ApplicationController
 
   def tag_tables
 
-    data = Rails.cache.fetch("database-metadata/tag-tables", expires_in: 8.hour) do
+    data = Rails.cache.fetch("database-metadata/tag-tables", expires_in: 7.days) do
       [
         { label: 'Work Tags', value: SubjectTag.where(taggable_type: 'Work').count(:id) },
         { label: 'Author Tags', value: SubjectTag.where(taggable_type: 'Author').count(:id) },
@@ -41,7 +41,7 @@ class Api::V1::Charts::DatabaseMetadataController < ApplicationController
 
   def token_tables
 
-    data = Rails.cache.fetch("database-metadata/token-tables", expires_in: 8.hour) do
+    data = Rails.cache.fetch("database-metadata/token-tables", expires_in: 7.days) do
       [
         { label: 'Work Tokens', value: Token.where(token_type: 'Work').count(:id) },
         { label: 'Author Tokens', value: Token.where(token_type: 'Author').count(:id) },
